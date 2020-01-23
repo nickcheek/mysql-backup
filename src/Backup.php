89bcd4372724b743e49a5d003bbb7a2292026ae5
Nick <nick@nicholascheek.com>
2020-01-23 12:31:53 -0600

    first commit

--- src/Backup.php
+++ src/Backup.php
@@ -13,26 +13,57 @@ class Backup {
     public function __construct($key)
     {
         $this->dropbox = new Dropbox\Dropbox($key);
-        $this->db = "live";
-        $this->user = "ncheek";
-        $this->pass = "tristan5034";
-        $this->path = "/var/www/html/scripts/db-backup/";
+    }
+
+    public function setUsername($username)
+    {
+        $this->user = username;
+    }
+
+    public function setPassword($password)
+    {
+        $this->pass = $password;
+    }
+
+    public function setDatabase($database)
+    {
+        $this->db = $database;
+    }
+
+    public function setPath($path)
+    {
+        $this->path = $path;
     }
 
     public function getBackup()
     {
+        $vars = array($this->user, $this->pass,$this->db,$this->path);
+        if(!isset($vars)) {
+            echo 'One or more required variables have not been set';
+            exit;
+        }
         $cmd = "mysqldump --hex-blob --routines --skip-lock-tables --log-error=mysqldump_error.log -u {$this->user} -p{$this->pass} {$this->db} > " . $this->path . "{$this->db}-backup.sql";
         exec($cmd);
     }
 
     public function zipFile()
     {
+        $vars = array($this->db,$this->path);
+        if(!isset($vars)) {
+            echo 'One or more required variables have not been set';
+            exit;
+        }
         exec("zip " . $this->path . "{$this->db}.zip " . $this->path . "{$this->db}-backup.sql");
         unlink($this->path.$this->db."-backup.sql");
     }
 
     public function uploadFile()
     {
+        $vars = array($this->db,$this->path);
+        if(!isset($vars)) {
+            echo 'One or more required variables have not been set';
+            exit;
+        }
         $this->dropbox->files->upload("/DB-Backups/{$this->db}.zip" , $this->path . "{$this->db}.zip");
         unlink($this->path.$this->db.".zip");
     }
